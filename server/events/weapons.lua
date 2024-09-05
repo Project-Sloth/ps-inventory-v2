054f9a3542457b48332298be72171fbbba34321c
Mustache Dom <123716453+Mustachedom@users.noreply.github.com>
2024-09-05 16:12:57 -0700

    Update weapons.lua

--- server/events/weapons.lua
+++ server/events/weapons.lua
@@ -1,11 +1,11 @@
 -- checks for ammo boxes table and removes the item and gives ammo 
-RegisterNetEvent(Config.ServerEventPrefix .. 'UseAmmoBox', function(itemremove, itemgain)
+RegisterNetEvent(Config.ServerEventPrefix .. 'UseAmmoBox', function(itemRemove, itemGain)
     local Player = Framework.Server.GetPlayer(source)
-    local check = Framework.Server.HasItem(source, itemremove, 1)
+    local check = Framework.Server.HasItem(source, itemRemove, 1)
     if not check then return end
-    local data = {item = Config.Weapons.AmmoBoxes[itemremove].item, amount = Config.Weapons.AmmoBoxes[itemremove].amount}
+    local data = {item = Config.Weapons.AmmoBoxes[itemRemove].item, amount = Config.Weapons.AmmoBoxes[itemRemove].amount}
     if data.item ~= itemgain then return end
-    if Core.Classes.Inventory.RemoveItem(source, itemremove, 1) then
+    if Core.Classes.Inventory.RemoveItem(source, itemRemove, 1) then
         Core.Classes.Inventory.AddItem(source, data.item, data.amount)
     end
 end)
